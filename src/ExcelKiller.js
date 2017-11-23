581bcbb2436a5cef7fca19e6caacf68d9f4ff4ed
vincent <chanyhot@hotmail.com>
2017-11-23 11:58:56 +0800

    [修改]支持多列图片下载整理

--- src/ExcelKiller.js
+++ src/ExcelKiller.js
@@ -6,11 +6,12 @@ var mkdirp = require('mkdirp');
 var Promise = require('promise');
 
 var ExcelKiller = function(filename) {
-    this.dir = path.join(__dirname, '../images');
+    this.dir = [path.join(__dirname, '../imagesFromDetail'), path.join(__dirname, '../imagesFromList')];
     this.export = path.join(__dirname, '../exportExcel.xlsx');
     this.list = filename ? xlsx.parse(path.join(__dirname, '../excel/') + filename + '.xlsx') : xlsx.parse(path.join(__dirname, '../excel/') + 'import.xlsx');
     this.findImgList = [];
-    this.num = 0;
+    this.howManyList = 0;
+    this.count = [];
 
     //下载图片
     this.download = function(url, dir, filename) {
@@ -26,8 +27,6 @@ var ExcelKiller = function(filename) {
     }
 
     var _this = this;
-    var srcStr;
-    var srcStrTemp = ''
 
     //删除生成的目录
     function deleteFolder(_path) {
@@ -55,7 +54,9 @@ var ExcelKiller = function(filename) {
 
     //重置
     function reset() {
-        deleteFolder(_this.dir);
+        for (var i = 0; i < _this.dir.length; i++) {
+            deleteFolder(_this.dir[i]);
+        }
         deleteFile(_this.export);
     }
 
@@ -66,11 +67,13 @@ var ExcelKiller = function(filename) {
 
     //创建目录
     function makeDir() {
-        mkdirp(_this.dir, function(err) {
-            if (err) {
-                console.log(err);
-            }
-        });
+        for (var i = 0; i < _this.dir.length; i++) {
+            mkdirp(_this.dir[i], function(err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
     }
 
     //整理出下载地址
@@ -87,24 +90,35 @@ var ExcelKiller = function(filename) {
         return getTimestamp().toString() + parseInt(Math.random() * 1000000).toString() + extName;
     };
 
-    reset();
-    setTimeout(function(){
-        makeDir();
+    function assetArray(num) {
+        _this.howManyList++;
+        _this.count.push(0);
+        var array = [];
+        _this.findImgList.push(array);
+        var srcStr;
+        var srcStrTemp = '';
         for (var i = 1; i < _this.list[0].data.length; i++) {
-            var findImg = _this.list[0].data[i][2].match(/src=\"https:\/\/.*?\"|src=\"\/\/.*?\"/g);
+            var findImg = _this.list[0].data[i][num].match(/src=\"https:\/\/.*?\"|src=\"\/\/.*?\"/g);
             if (findImg !== null) {
                 for (var j = 0; j < findImg.length; j++) {
                     var imgUrl = makeDownloadUrl(findImg[j])
                     var imageName = makeImageName(imgUrl.substr(-4, 4))
                     srcStr = '<img src="images/' + imageName + '">';
                     srcStrTemp += srcStr
-                    _this.findImgList.push([imgUrl, imageName]);
+                    _this.findImgList[_this.howManyList - 1].push([imgUrl, imageName]);
                 }
                 _this.list[0].data[i].push(srcStrTemp);
                 srcStrTemp = '';
             }
         }
-    },2000)
+    }
+
+    reset();
+    setTimeout(function() {
+        makeDir();
+        assetArray(3);
+        assetArray(4);
+    }, 2000);
 }
 
 //写excel文件
@@ -115,18 +129,20 @@ ExcelKiller.prototype.writeExcel = function() {
 };
 
 //下载图片
-ExcelKiller.prototype.downloadImages = function() {
+ExcelKiller.prototype.downloadImages = function(num) {
     if (this.findImgList.length !== 0) {
         var _this = this;
-        this.download(this.findImgList[this.num][0], this.dir, this.findImgList[this.num][1]).then(function() {
-            if (_this.num === _this.findImgList.length) {
-                _this.num = 0;
-                return;
-            } else {
-                _this.num++;
-                _this.downloadImages();
-            }
-        });
+        if (this.findImgList[num].length !== 0) {
+            this.download(this.findImgList[num][this.count[num]][0], this.dir[num], this.findImgList[num][this.count[num]][1]).then(function() {
+                if (_this.count[num] === _this.findImgList[num].length) {
+                    _this.count[num] = 0;
+                    return;
+                } else {
+                    _this.count[num]++;
+                    _this.downloadImages(num);
+                }
+            });
+        }
     }
 };
 
